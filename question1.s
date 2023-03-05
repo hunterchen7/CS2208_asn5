bb6e2028e88e0b5a5ed0fa99cda032f7f504514c
Hunter Chen <0x@hunterchen.ca>
2023-03-05 16:32:19 -0500

    Update question1.s

--- question1.s
+++ question1.s
@@ -1,6 +1,6 @@
      AREA prog, CODE, READONLY
      ENTRY
-     MOV r0,#2 ; default set to false
+     MOV r0,#2 			; default set to false
 UPC	 DCB "013800150738" ; UPC code
-	 LDRB r1,UPC ; get first byte of UPC code
+	 LDRB r1,UPC 		; get first byte of UPC code
      END
