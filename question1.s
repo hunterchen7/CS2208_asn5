49ea2f8a6a7c2a200241e459f3df89a22f0f73a7
Hunter Chen <0x@hunterchen.ca>
2023-03-07 11:48:43 -0500

    Update question1.s

--- question1.s
+++ question1.s
@@ -9,9 +9,9 @@ Sum     LDRB r4,[r0]        ; put even numbers(s) in r4 0,2,4,6,8,10
         ADD r4,r4,r4,LSL#1  ; multiply the even number (r4) by 3
         ADD r1,r1,r4        ; add even byte to check sum
         ADD r1,r1,r5        ; add odd byte to check sum, note that the check digit is included here and added as an odd number
-        ADDS r0,r0,#2       ; increment by 2 since we're loading 2 bytes at once
+        ADD r0,r0,#2        ; increment by 2 since we're loading 2 bytes at once
         CMP r0,r2           ; check if we're still in the loop
-        BNE Sum				; continue looping if r0 is not equal to r2, i.e. pointer doesn't point to the end
+        BNE Sum             ; continue looping if r0 is not equal to r2, i.e. pointer doesn't point to the end
         MOV r0,#2           ; set r0 to a default value of false
 Mod10   SUBS r1,r1,#10      ; subtract 10 from r1 and set flags, looking for Z flag to set r0 to 1, and N flag to exit loop
         MOVEQ r0,#1         ; set r0 to true if we have r1 = 0, which means the check sum is divisible by 10
