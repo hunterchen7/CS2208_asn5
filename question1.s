76d0bc16a8441a4388ba182c917b4528033ac7fa
Hunter Chen <0x@hunterchen.ca>
2023-03-14 23:13:21 -0400

    Update question1.s

--- question1.s
+++ question1.s
@@ -1,7 +1,7 @@
         AREA Asn5, CODE, READONLY
         ENTRY
         ADR r0,UPC          ; point r0 to UPC, address of the current character from the UPC being accessed
-        LDR r1,=-0x480      ; check sum, -0x480 accounts for the 0x30s in front of our ascii numerals, we add 6*0x30*3 for even bytes, 6*0x30 for odd bytes, 6*0x30*3+6*0x30 = 0x480 = 1152 
+        LDR r1,=-0x476      ; check sum, -0x480 accounts for 0x30s, 6*0x30*3 evens, 6*0x30 odds, 6*0x30*3+6*0x30 = 0x480 = 1152, then +10 so UPC code of all 0 is valid.
         ADD r2,r0,#12       ; put where the loop finishes into r2, 12 characters total, 6 odd, 6 even
 Sum     LDRB r4,[r0],#1     ; put even number in r4 0,2,4,6,8,10, then index UPC pointer by 1
         LDRB r5,[r0],#1     ; put odd number in r5 1,3,5,7,9,11, then index UPC pointer by 1
@@ -15,5 +15,5 @@ Mod10   SUBS r1,r1,#10      ; subtract 10 from r1 and set flags, looking for Z f
         MOVEQ r0,#1         ; set r0 to true if we have r1 = 0, which means the check sum is divisible by 10
         BPL Mod10           ; continue the loop while r1 is positive or 0, which means exit when negative
 Loop    B Loop
-UPC     DCB "067000104015"  ; UPC code
+UPC     DCB "056000004609"  ; UPC code
         END
